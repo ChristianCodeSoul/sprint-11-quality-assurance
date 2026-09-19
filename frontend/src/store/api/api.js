5e8153cc4d9b913de0b2195c989ef2141fd1dceb
Mansha Pandey <manshabandhu@gmail.com>
2026-09-19 13:19:56 +0530

    Use environment variable for API base URL

--- frontend/src/store/api/api.js
+++ frontend/src/store/api/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:4000/api",
+        baseUrl: process.env.NEXT_PUBLIC_API_URL,
     }),
     tagTypes: ["Product", "User", "Order"],
 
